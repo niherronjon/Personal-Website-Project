423e68442da52fbdd2a9f24ac73f3d22aaffd2b4
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-05-24 18:11:06 +0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Interactive Button
-document.getElementById("cta-button").addEventListener("click", () => {
-    alert("Button clicked! 🎉");
-    console.log("Action triggered!");
-});
-
-// You can add more JavaScript functionality here
-// Example: Fetch API, animations, etc.
